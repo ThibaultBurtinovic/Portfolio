209aa902d29114f2c4329a893177e6771e070006
Thibault Burtinovic <thibault.burtinovic@gmail.com>
2023-12-14 16:48:07 +0100

    Updates

---
+++ infoProjets.jsx
@@ -0,0 +1,18 @@
+
+
+export const infoProjets = [
+	{
+		"id": "6",
+		"title": "Booki",
+		"pictures": [
+			// "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+			// "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg",
+			// "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg",
+			// "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-4.jpg",
+			// "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg"
+		],
+
+		"description": "Créez la page d'accueil d'une agence de voyage avec HTML & CSS",
+		
+	}	
+]
